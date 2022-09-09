c8492c682235e40b29446d75f2efef15e33d9dc5
Clément Petit <clement.petit@glovoapp.com>
2022-09-09 12:11:12 +0200

    Update structured concurrency

--- src/main/java/com/glovoapp/demo/StructuredConcurrencyDemo.java
+++ src/main/java/com/glovoapp/demo/StructuredConcurrencyDemo.java
@@ -10,20 +10,14 @@ public class StructuredConcurrencyDemo {
   public static void main(String[] args) throws Exception {
     System.err.printf("Starting %s virtual threads !%n", NUM_THREADS);
 
-    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
 
-      final var call1 = executor.submit(WorkLoads::callApi);
-      final var call2 = executor.submit(WorkLoads::callApi);
+        final var call1 = executor.submit(WorkLoads::callApi);
+        final var call2 = executor.submit(WorkLoads::callApi);
 
       final var result1 = call1.get();
       final var result2 = call2.get();
       System.err.printf("Results are: %s %s%n", result1, result2);
-
-      for (int i = 0; i < NUM_THREADS; i++) {
-        executor.submit(WorkLoads::callApi);
-      }
-      System.err.println("All virtual threads started !");
-      System.err.println("Waiting on all virtual threads!");
     }
 
     System.err.println("Done waiting on all virtual threads!");
